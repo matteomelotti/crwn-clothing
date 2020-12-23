bd057a70b1a63d5faaf65153748ae0f8e5665c64
Matteo Melotti <matteo.melotti11@gmail.com>
2020-12-23 23:51:59 +0100

    broken homepage into smaller components, finished styling menu-item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
+
 import './App.css';
-import HomePage from './homepege.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
