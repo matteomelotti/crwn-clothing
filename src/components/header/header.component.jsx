a630d6719c77e0f59bac832b0ee7f5e43136ba33
Matteo Melotti <matteo.melotti11@gmail.com>
2020-12-29 19:19:00 +0100

    fix header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { ReactComponent as Logo } from './crown.svg';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 import './header.styles.scss';
 
 const Header = () => (
   <div className='header'>
-    <Link to='/' className='logo-container'>
+    <Link className='logo-container' to='/'>
       <Logo className='logo' />
     </Link>
     <div className='options'>
@@ -18,6 +19,6 @@ const Header = () => (
       </Link>
     </div>
   </div>
-)
+);
 
 export default Header;
