1a0c21da65563cd455e482c699c136f8e2256bac
Matteo Melotti <matteo.melotti11@gmail.com>
2021-02-04 18:52:32 +0100

    stripe payment

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51IHBgzJYL3NzAYUDRzuJNqdKDS2aREzSACezxijKjbuoGvxnYiv3iegAzzhMNHdJt3VeU76cZ9TIIvJT6DnKlkSP009zRnUHcu';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
